3a04351043753f2151fcab6f2cb6bae28686992c
Mateusz Dobija <mateusz.dobija.rce@gmail.com>
2021-02-04 00:00:45 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header className="grid-container__header">
+        <a href="/">React Shopping cart</a>
       </header>
+      <main className="grid-container__main">Product list</main>
+      <footer className="grid-container__footer">
+        All rights is reserved.
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
